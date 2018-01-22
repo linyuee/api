b287ceff7b0eb66da3d9c9db608f329ddca5fb4d
linyue <769353695@qq.com>
2018-01-22 11:43:26 +0800

    init

---
+++ src/Auth/Jwt.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuelin
+ * Date: 2018/1/20
+ * Time: 下午6:57
+ */
+
+class Jwt
+{
+    private static $expires = 60; //设置单次过期时间(分钟)
+    private static $appExpires = 60*24*30;
+
+    public function createToken(array $session_data = array(),$type = 'app')
+    {
+
+        $session_key = guid();
+
+
+        $device = 'web';
+        $time = time();
+        $expires_time = $time + (self::$expires * 60);
+        $secret_key = '';
+
+        if ($type == 'app') {
+            $device = 'app';
+            $expires_time = $time + (self::$appExpires * 60);
+            $secret_key = guid();
+        }
+        $session_data['device'] = $device;
+        $session_data['create_at'] = $time;      //创建时间
+        $session_data['expires_time'] = $expires_time;  //过期时间
+        $session_data['sk'] = $secret_key;
+
+        $data = array(
+            'session_key' => $session_key
+        );
+        //过期时间
+        $session_expires_time = $device == 'web' ? self::$expires : self::$appExpires;
+
+        \Cache::add('session:' . $session_key, $session_data, $session_expires_time);
+        return ['token' => JWT::encode($data, config('app.key')), 'sk' => $secret_key];
+    }
+}
---
+++ src/Exception/ExceptionHandler.php
@@ -0,0 +1,89 @@
+<?php
+namespace Linyuee;
+/**
+ *
+ */
+class ExceptionHandler extends \Illuminate\Foundation\Exceptions\Handler
+{
+
+    static $httpVersion = "HTTP/1.1";
+    static $contentType = 'application/json';
+
+    public static function getHttpStatusMessage($statusCode){
+        $httpStatus = array(
+            100 => 'Continue',
+            101 => 'Switching Protocols',
+            200 => 'OK',
+            201 => 'Created',
+            202 => 'Accepted',
+            203 => 'Non-Authoritative Information',
+            204 => 'No Content',
+            205 => 'Reset Content',
+            206 => 'Partial Content',
+            300 => 'Multiple Choices',
+            301 => 'Moved Permanently',
+            302 => 'Found',
+            303 => 'See Other',
+            304 => 'Not Modified',
+            305 => 'Use Proxy',
+            306 => '(Unused)',
+            307 => 'Temporary Redirect',
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            402 => 'Payment Required',
+            403 => 'Forbidden',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            406 => 'Not Acceptable',
+            407 => 'Proxy Authentication Required',
+            408 => 'Request Timeout',
+            409 => 'Conflict',
+            410 => 'Gone',
+            411 => 'Length Required',
+            412 => 'Precondition Failed',
+            413 => 'Request Entity Too Large',
+            414 => 'Request-URI Too Long',
+            415 => 'Unsupported Media Type',
+            416 => 'Requested Range Not Satisfiable',
+            417 => 'Expectation Failed',
+            500 => 'Internal Server Error',
+            501 => 'Not Implemented',
+            502 => 'Bad Gateway',
+            503 => 'Service Unavailable',
+            504 => 'Gateway Timeout',
+            505 => 'HTTP Version Not Supported');
+        return isset($httpStatus[$statusCode])?$httpStatus[$statusCode]:$httpStatus[500];
+
+    }
+
+    public static function handler(\Exception $exception){
+        if (PHP_SAPI == 'cli'){
+            parent::report($exception);
+        }else{
+            self::response($exception);
+        }
+
+    }
+
+    public static function response($exception){
+        header("Content-Type:". self::$contentType);
+        $statusCode = method_exists($exception,'getStatusCode')?$exception->getStatusCode():$exception->getCode();
+        header(self::$httpVersion. " ". $statusCode ." " . self::getHttpStatusMessage($statusCode));
+        $response = array(
+            'data'=>[],
+            'status_code'=>$statusCode == 0?500:$statusCode,
+            'message'=>'',
+            'error_id'=>method_exists($exception,'getErrorId')?$exception->getErrorId():self::getHttpStatusMessage($statusCode),
+        );
+        if (env('APP_DEBUG')){
+            $response = array_merge($response,array('debug'=>array(
+                'file'=>$exception->getFile(),
+                'line'=>$exception->getLine(),
+                'error_msg'=>$exception->getMessage(),
+                'trace'=>$exception->getTrace(),
+                'type'=>get_class($exception)
+            )));
+        }
+        echo json_encode($response);die();
+    }
+}
